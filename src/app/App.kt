f8bbb2d63899ff62084d56c44277816a7775700a
S. Matthew English <s.matthew.english@gmail.com>
2020-01-03 12:59:05 -0500

    looks good

--- src/app/App.kt
+++ src/app/App.kt
@@ -10,7 +10,7 @@ class App : RComponent<RProps, RState>() {
         div("App-header") {
             logo()
             h2 {
-                +"Welcome to React with Kotlin"
+                +"tschess"
             }
         }
         p("App-intro") {
